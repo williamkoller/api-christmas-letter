77a93dcc13051cf387a3876c49d0b781cf632903
william koller <wkoller25@gmail.com>
2021-06-18 23:44:53 -0300

    feat: add configs for core and common

---
+++ src/common/filters/model-not-found.exception.filter.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
+
+@Catch(EntityNotFoundError)
+export class ModelNotFoundException implements ExceptionFilter {
+  catch(exception: EntityNotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    return response.status(HttpStatus.NOT_FOUND).json({
+      error: {
+        error: 'Not Found',
+        message: exception.message,
+      },
+    });
+  }
+}
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,15 @@
+import { configService } from '@/infra/database/config/config.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
+  ],
   controllers: [],
   providers: [],
 })
