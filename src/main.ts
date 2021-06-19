e5ed74b140fadc2aded6c8e1ad664b9b8999e3e5
william koller <wkoller25@gmail.com>
2021-06-18 23:49:16 -0300

    feat: add configs initial in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor';
+import { ModelNotFoundException } from '@/common/filters/model-not-found.exception.filter';
+
+const key = 'fetch';
+
+global[key] = require('node-fetch');
+
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+  const port = process.env.PORT;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new ModelNotFoundException());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  await app.listen(port, () => logger.log(`App running 🔥`));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
