99dcfabb953a2564fed2db4f390103aec2304938
william koller <wkoller25@gmail.com>
2021-06-19 21:10:07 -0300

    refactor: rename method hasherComparer to comparer

--- src/infra/cryptography/hash-comparer/hash-comparer.ts
+++ src/infra/cryptography/hash-comparer/hash-comparer.ts
@@ -3,7 +3,7 @@ import { compareSync } from 'bcrypt';
 
 @Injectable()
 export class HashComparer {
-  async hashComparer(password: string, hash: string): Promise<boolean> {
+  async comparer(password: string, hash: string): Promise<boolean> {
     return compareSync(password, hash);
   }
 }
