4e2cf6b89748a3a15de46afaf6bfa44f0d0ea0d3
william koller <wkoller25@gmail.com>
2021-06-19 22:34:57 -0300

    feat: add AuthModule in AppModlue

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '@/modules/user/user.module';
 import { CoreModule } from '@/modules/core/core.module';
+import { AuthModule } from '@/modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CoreModule } from '@/modules/core/core.module';
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
     forwardRef(() => UserModule),
     forwardRef(() => CoreModule),
+    forwardRef(() => AuthModule),
   ],
   controllers: [],
   providers: [],
