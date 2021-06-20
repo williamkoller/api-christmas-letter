ff68d5943d00ef00f037bb25ddd257f6d1933393
william koller <wkoller25@gmail.com>
2021-06-19 21:14:37 -0300

    feat: add delete user  in service

---
+++ src/modules/user/services/delete-user/delete-user.service.ts
@@ -0,0 +1,21 @@
+import { Message } from '@/utils/types/message/message.type';
+import { LoadByIdDto } from '@/modules/user/dtos/load-by-id/load-by-id.dto';
+import { DeleteUserRepository } from '@/modules/user/repositories/delete-user/delete-user.repository';
+import { LoadUserByIdService } from '@/modules/user/services/load-user-by-id/load-user-by-id.service';
+
+export class DeleteUserService {
+  constructor(
+    private readonly loadUserByIdService: LoadUserByIdService,
+    private readonly deleteUserRepository: DeleteUserRepository,
+  ) {}
+
+  async deleteUser({ id }: LoadByIdDto): Promise<Message> {
+    const { id: userId } = await this.loadUserByIdService.loadById({ id });
+
+    await this.deleteUserRepository.deleteUser({ id: userId });
+
+    return {
+      message: 'User deleted with successfully',
+    };
+  }
+}
