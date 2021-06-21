0c96bf19a4234bd8bf3256544eb7703d20611c4e
william koller <wkoller25@gmail.com>
2021-06-21 14:13:45 -0300

    test: should be returns created data in AddCardRepoisitory

--- src/modules/card/repositories/add-card/add-card.repository.spec.ts
+++ src/modules/card/repositories/add-card/add-card.repository.spec.ts
@@ -3,15 +3,36 @@ import { AddCardRepository } from './add-card.repository';
 
 describe('AddCardRepository', () => {
   let repository: AddCardRepository;
-
+  let mockData;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AddCardRepository],
     }).compile();
 
     repository = module.get<AddCardRepository>(AddCardRepository);
+    mockData = {
+      title: 'any title',
+      description: 'any description',
+      destiny: 'any destiny',
+      date: '06/21/2021',
+      userId: '5a84a0fc-297c-4e09-840d-fe22b4ff94d5',
+    };
+    repository.save = jest.fn();
   });
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should be called save with correct params', async () => {
+    const newCard = (repository.create = jest.fn().mockReturnValue(mockData));
+    repository.save = jest.fn().mockReturnValue(newCard);
+    await repository.add(mockData);
+    expect(repository.save).toBeCalledWith(mockData);
+  });
+
+  it('should be returns created data', async () => {
+    const newCard = (repository.create = jest.fn().mockReturnValue(mockData));
+    (repository.save as jest.Mock).mockReturnValue(newCard);
+    expect(await repository.add(mockData)).toEqual(newCard);
   });
 });
