b5a645992b941af987862e47e21337d9d46206c5
william koller <wkoller25@gmail.com>
2021-06-21 02:21:42 -0300

    refactor: add ApiProperty in entities

--- src/infra/database/entities/card/card.entity.ts
+++ src/infra/database/entities/card/card.entity.ts
@@ -1,21 +1,27 @@
-import { Column, Entity, JoinColumn, JoinTable, ManyToOne } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { BaseEntity } from '../base-entity/base.entity';
 import { User } from '../user/user.entity';
 
 @Entity('cards')
 export class Card extends BaseEntity {
+  @ApiProperty()
   @Column('varchar')
   title: string;
 
+  @ApiProperty()
   @Column('varchar')
   description: string;
 
+  @ApiProperty()
   @Column('varchar')
   destiny: string;
 
+  @ApiProperty()
   @Column('date')
   date: Date;
 
+  @ApiProperty()
   @ManyToOne(() => User, (user) => user.cards)
   user: User;
 
--- src/infra/database/entities/user/user.entity.ts
+++ src/infra/database/entities/user/user.entity.ts
@@ -1,21 +1,27 @@
 import { BaseEntity } from '../base-entity/base.entity';
 import { Column, Entity, JoinColumn, JoinTable, OneToMany } from 'typeorm';
 import { Card } from '../card/card.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('users')
 export class User extends BaseEntity {
+  @ApiProperty()
   @Column('varchar')
   name: string;
 
+  @ApiProperty()
   @Column('varchar')
   surname: string;
 
+  @ApiProperty()
   @Column('varchar')
   email: string;
 
+  @ApiProperty()
   @Column('varchar')
   password: string;
 
+  @ApiProperty()
   @OneToMany(() => Card, (card) => card.user, { eager: true })
   @JoinTable()
   cards: Card[];
